42433da51d9955a59b42c68815bcbb0999431c4b
James O'Keeffe <jamesokeeffe50@gmail.com>
2024-05-19 11:29:10 +0800

    Fix console errors

--- app/ui/svg/ImageToImageIcon.tsx
+++ app/ui/svg/ImageToImageIcon.tsx
@@ -9,7 +9,7 @@ const ImageToImageIcon = () => {
       opacity="1"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M3.97134 13.4444C2.71235 13.4444 1.68873 12.3981 1.68873 11.1111C1.68873 9.82415 2.71235 8.77778 3.97134 8.77778H8.02931C10.1273 8.77778 11.8337 7.03348 11.8337 4.88889C11.8337 2.7443 10.1273 1 8.02931 1H6.00033C5.58033 1 5.23946 1.34844 5.23946 1.77778C5.23946 2.20711 5.58033 2.55556 6.00033 2.55556H8.02931C9.2883 2.55556 10.3119 3.60193 10.3119 4.88889C10.3119 6.17585 9.2883 7.22222 8.02931 7.22222H3.97134C1.87337 7.22222 0.166992 8.96652 0.166992 11.1111C0.166992 13.2557 1.87337 15 3.97134 15H6.00033C6.42033 15 6.7612 14.6516 6.7612 14.2222C6.7612 13.7929 6.42033 13.4444 6.00033 13.4444H3.97134Z"
         fill="url(#image-guidance-icon-linear-gradient-1-:r15b:)"
@@ -17,7 +17,7 @@ const ImageToImageIcon = () => {
       <path
         d="M9.0641 12.6H9.0539C8.27489 12.6 7.63944 13.2224 7.63944 14C7.63944 14.7827 8.29036 15.4 9.0641 15.4C9.84047 15.4 10.4837 14.7802 10.4837 14C10.4837 13.2198 9.84047 12.6 9.0641 12.6ZM2.94165 0.6H2.93145C2.15244 0.6 1.51699 1.22241 1.51699 2C1.51699 2.78275 2.16791 3.4 2.94165 3.4C3.71802 3.4 4.36121 2.78018 4.36121 2C4.36121 1.21982 3.71802 0.6 2.94165 0.6Z"
         stroke="url(#image-guidance-icon-linear-gradient-2-:r15b:)"
-        stroke-width="0.8"
+        strokeWidth="0.8"
       ></path>
       <defs>
         <linearGradient
@@ -28,9 +28,9 @@ const ImageToImageIcon = () => {
           y2="2.73062"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#DB519E"></stop>
-          <stop offset="0.499028" stop-color="#B14BF4"></stop>
-          <stop offset="1" stop-color="#666BD1"></stop>
+          <stop stopColor="#DB519E"></stop>
+          <stop offset="0.499028" stopColor="#B14BF4"></stop>
+          <stop offset="1" stopColor="#666BD1"></stop>
         </linearGradient>
         <linearGradient
           id="image-guidance-icon-linear-gradient-2-:r15b:"
@@ -40,9 +40,9 @@ const ImageToImageIcon = () => {
           y2="3.34618"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#DB519E"></stop>
-          <stop offset="0.499028" stop-color="#B14BF4"></stop>
-          <stop offset="1" stop-color="#666BD1"></stop>
+          <stop stopColor="#DB519E"></stop>
+          <stop offset="0.499028" stopColor="#B14BF4"></stop>
+          <stop offset="1" stopColor="#666BD1"></stop>
         </linearGradient>
       </defs>
     </svg>
